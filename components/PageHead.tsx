9b190e8d52dc6d1f6540f46ecb7d4b25e66a11bc
lucascervera <lucas@lucascervera.com>
2022-11-02 20:55:41 +0100

    Config

---
+++ components/PageHead.tsx
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import * as React from 'react'
+import * as types from 'lib/types'
+
+// TODO: remove duplication between PageHead and NotionPage Head
+
+export const PageHead: React.FC<types.PageProps> = ({ site }) => {
+  return (
+    <Head>
+      <meta charSet='utf-8' />
+      <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
+      <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1, shrink-to-fit=no'
+      />
+
+      {site?.description && (
+        <>
+          <meta name='description' content={site.description} />
+          <meta property='og:description' content={site.description} />
+        </>
+      )}
+
+      <meta name='theme-color' content='#EB625A' />
+      <meta property='og:type' content='website' />
+      
+             <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=G-KN64N6NYW9`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-CODE', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+      
+    </Head>
+  )
+}
